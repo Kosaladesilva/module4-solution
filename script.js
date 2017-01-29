0b7ec4648af55dd0567419331562c687d97e226a
kosaladesilva <kosaladesilva@yahoo.com>
2017-01-29 22:03:45 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/(function (window) {
+(function (window) {
 // *******************************
 // START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
 // *******************************
@@ -21,11 +21,13 @@ Hello Larry
 Hello Paula
 Hello Laura
 Good Bye Jim
+
 WARNING!!! WARNING!!!
 The code does NOT currently work! It is YOUR job to make it work
 as described in the requirements and the steps in order to complete this
 assignment.
 WARNING!!! WARNING!!!
+
 */
 
 // STEP 1:
